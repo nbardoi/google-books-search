4d9bf8c43f5e7b880eb62d9d94069389b4902ea6
Nagma Bardoi <nagmabardoi@gmail.com>
2020-02-29 21:35:38 -0600

    heroku

---
+++ client/build/precache-manifest.4ae0e73a767069cf5e43393cdb4ec9bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c62f0c0499c7400ce9d",
+    "url": "/static/css/main.f14fc487.chunk.css"
+  },
+  {
+    "revision": "7c62f0c0499c7400ce9d",
+    "url": "/static/js/main.101f64e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66fe564be5d94d324f84",
+    "url": "/static/js/2.435f5b41.chunk.js"
+  },
+  {
+    "revision": "c154bff97378dcfd4b20803359696de4",
+    "url": "/index.html"
+  }
+];
